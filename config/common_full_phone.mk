238a0f0cc80457344591373a5e437130b6aa4617
mnemonyc <mnemonyc@hotmail.com>
2017-12-30 19:34:07 +0100

    Reorder discovery vendor for new extra vendor

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -2,6 +2,10 @@
 $(call inherit-product, vendor/discovery/config/common.mk)
 $(call inherit-product, vendor/discovery/config/common_apn.mk)
 
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/discovery/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+
 # Telephony 
 PRODUCT_PACKAGES += \
     Stk 
