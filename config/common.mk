TheXPerienceProject/android_vendor_xperience

[2/2] Add 'GoogleSans' font family
Change-Id: Ic8c8a18a34c422353202382b488ccef992f6f89e
Signed-off-by: klozz <klozz707@gmail.com>

config/common.mk
@@ -82,6 +82,14 @@ PRODUCT_COPY_FILES += \
     vendor/xperience/prebuilt/common/bin/50-xpe.sh:system/addon.d/50-xpe.sh \
     vendor/xperience/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/xperience/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
 
 #Falcon Tweaking
 ifeq ($(XPE_BUILD), falcon)

DiscoveryTeam/proprietary_vendor_discovery

config/common.mk
@@ -24,6 +24,15 @@ PRODUCT_COPY_FILES += \
     vendor/discovery/tools/backuptool.functions:install/bin/backuptool.functions \
     vendor/discovery/tools/50-discovery.sh:system/addon.d/50-discovery.sh
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/discovery/prebuilt/common/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
 # Backup services whitelist
 PRODUCT_COPY_FILES += \
     vendor/discovery/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
@@ -55,7 +64,7 @@ PRODUCT_COPY_FILES += \
 
 # Fix Dialer
 PRODUCT_COPY_FILES +=  \
-    vendor/discovery/prebuilt/common/sysconfig/dialer_experience.xml:system/etc/sysconfig/dialer_experience.xml 
+    vendor/discovery/prebuilt/common/sysconfig/dialer_discovery.xml:system/etc/sysconfig/dialer_discovery.xml 
 
 # discovery-specific startup services
 PRODUCT_COPY_FILES += \